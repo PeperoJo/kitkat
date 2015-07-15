5e2f92443c9a09b8c79e7478b8a9b550acd8e80e
josephhan11 <sjhan123@gmail.com>
2015-07-15 18:30:08 +0000

    changes

---
+++ apparel/chestware/chestware.rb
@@ -0,0 +1,18 @@
+class Chestware
+  chestware = [
+    #number 1
+    polo => {
+      gender = 'male';
+      occasion = 'formal'
+      temp_low = '50'; #Farenheit
+      temp_high = '70'; #Farenheit
+      }
+    #number 2
+    blouse => {
+      gender = 'female';
+      occasion = 'casual'
+      temp_low = '30'; #Farenheit
+      temp_high = '80'; #Farenheit
+      }
+    ]
+end
