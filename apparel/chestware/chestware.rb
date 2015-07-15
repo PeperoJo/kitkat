b891ffc575e5b39b6f2014c9966385614c7e0ab1
josephhan11 <sjhan123@gmail.com>
2015-07-15 18:41:55 +0000

    changes

--- apparel/chestware/chestware.rb
+++ apparel/chestware/chestware.rb
@@ -1,20 +1,20 @@
 class Chestware
-  chestware = [
+  chestware = {}
     #number 1
-    polo = {
-      :name => 'Ralph Lauren: Polo'
-      :gender => 'male';
-      :occasion => 'formal'
-      :temp_low => '50'; #Farenheit
-      :temp_high => '70'; #Farenheit
-      }
+    polo => {
+      :name => 'Ralph Lauren: Polo',
+      :gender => 'male',
+      :occasion => 'formal',
+      :temp_low => '50', #Farenheit
+      :temp_high => '70', #Farenheit
+      },
     #number 2
     blouse = {
-      :name => 'Forever 21: Blouse'
-      :gender => 'female';
-      :occasion => 'casual'
-      :temp_low => '30'; #Farenheit
-      :temp_high => '80'; #Farenheit
+      :name => 'Forever 21: Blouse',
+      :gender => 'female',
+      :occasion => 'casual',
+      :temp_low => '30', #Farenheit
+      :temp_high => '80', #Farenheit
       }
-    ]
+  }
 end
